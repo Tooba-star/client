4a048bdb1c03bf9698e7091787a9949317faaee8
Tooba-star <toobaanwer32@gmail.com>
2021-01-11 12:11:27 +0500

    Login-Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [ state, setstate] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:4000/api/users')
+    .then(res=>res.json())
+    .then(res=>{
+      console.log(res)
+      setstate(res)
+    })
+    .catch(e=>console.log(e))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        state.map((item)=>(
+          <div key={item.id}>{item.name}</div>
+        ))
+      }
     </div>
   );
 }
